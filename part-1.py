103134b5bf8f4714fb82694184c9a08bacf4d567
Francois Gervais <francoisgervais@gmail.com>
2018-05-18 22:35:03 -0400

    In the end, made part-1.py like the example

--- part-1.py
+++ part-1.py
@@ -15,41 +15,41 @@ def pull_bandit(bandit):
     result = np.random.randn(1)
     if result > bandit:
         #return a positive reward.
-        return [1]
+        return 1
     else:
         #return a negative reward.
-        return [-1]
+        return -1
 
 
-def build_model(x):
+def build_model():
     # model = tf.layers.Dense(10, activation=tf.nn.relu)(x)
-    model = tf.layers.Dense(len(bandits), use_bias=False,
-        kernel_initializer=
-            tf.initializers.ones())(x)
-    # model = tf.Variable(tf.random_uniform([16,4],0,0.01))
+    # model = tf.layers.Dense(len(bandits), use_bias=False,
+    #     kernel_initializer=
+    #         tf.initializers.ones())(x)
+    model = tf.Variable(tf.random_uniform([num_bandits], 0, 0.01))
     # model = tf.matmul(x, model)
     return model
 
 
-reward_holder = tf.placeholder(shape=[1, 1], dtype=tf.float32)
-action_holder = tf.placeholder(shape=[1, 1], dtype=tf.int32)
+reward_holder = tf.placeholder(shape=[1], dtype=tf.float32)
+action_holder = tf.placeholder(shape=[1], dtype=tf.int32)
 
 # with tf.Session() as sess:
 #     print(sess.run(tf.one_hot(indices=[0], depth=16)))
 #     print(sess.run(tf.zeros([1,1])))
 # exit(0)
 
-model = build_model(tf.ones([1,1]))
-chosen_action = tf.argmax(input=model, axis=1)
+model = build_model()
+chosen_action = tf.argmax(input=model)
 
 # with tf.Session() as sess:
 #     init = tf.global_variables_initializer()
 #     sess.run(init)
-#     print(tf.squeeze(model))
+#     print(model)
 #     print(action_holder)
 # exit(0)
 
-responsible_weight = model.slice(action_holder FIXME
+responsible_weight = model[tf.squeeze(action_holder)]
 loss = -(tf.log(responsible_weight) * reward_holder)
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
@@ -60,7 +60,7 @@ init = tf.global_variables_initializer()
 
 
 
-total_episodes = 10000 # Set total number of episodes to train agent on.
+total_episodes = 1000 # Set total number of episodes to train agent on.
 total_reward = np.zeros(num_bandits) # Set scoreboard for bandits to 0.
 e = 0.1 # Set the chance of taking a random action.
 
@@ -72,7 +72,7 @@ with tf.Session() as sess:
     while i < total_episodes:
         # Choose either a random action or one from our network.
         if np.random.rand(1) < e:
-            action = [np.random.randint(num_bandits)]
+            action = np.random.randint(num_bandits)
         else:
             action = sess.run(chosen_action)
 
@@ -93,8 +93,8 @@ with tf.Session() as sess:
 
 print(ww)
 print("The agent thinks bandit {} is the most promising".format(
-                                                    np.argmax(ww, 1)+1))
-if np.argmax(ww, 1) == np.argmax(-np.array(bandits)):
+                                                    np.argmax(ww)+1))
+if np.argmax(ww) == np.argmax(-np.array(bandits)):
     print("...and it was right!")
 else:
     print("...and it was wrong!")
